d4363478851a3ac9735d6a49ba2469920744629a
aksoy-dev <aksoyfirat168@gmail.com>
2022-11-02 21:02:32 +0300

    React employee management

---
+++ api/Models/Employees.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models
+{
+    public class Employees
+    {
+        //prop shortcut
+        public int EmployeeID { get; set; }
+        public string EmployeeName { get; set; }
+        public string Department { get; set; }
+        public string MailId { get; set; }
+        public DateTime? DOJ { get; set; } //boş geçilebilir.
+    }
+}
